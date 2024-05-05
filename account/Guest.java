a9a3d4921369064c9418fdd86a9c9c457aad3b04
muhhabibie <144841901+muhhabibie@users.noreply.github.com>
2024-05-05 18:35:52 +0700

    Add files via upload

---
+++ account/Guest.java
@@ -0,0 +1,27 @@
+package Project;
+
+public class Guest extends Pelanggan {
+    public Guest(String firstName, String lastName, String NIK, String phoneNumber, String gender, String password) {
+        super(firstName, lastName, NIK, phoneNumber, gender, password);
+    }
+
+    @Override
+    public String getFullName() {
+        return firstName + (lastName != null ? " " + lastName : "");
+    }
+
+    @Override
+    public void makeOrder() {
+        System.out.println("Guest making an order");
+    }
+
+    @Override
+    public void confirmPay(int orderNumber) {
+        System.out.println("Guest confirming payment for order " + orderNumber);
+    }
+    @Override
+    public boolean isMember() {
+        return false;  // For Guest class
+    }
+
+}
